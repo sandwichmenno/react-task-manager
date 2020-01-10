6fda5200a4f4e8c4b8bf150e444a4ec2c4c5432f
Menno van Voorst <WAes43sxxCDkNZ>
2020-01-10 22:42:25 +0100

    Final version

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -6,11 +6,11 @@ import AddList from "./List/AddList/AddList";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-        width: 100%;
-        height: 100%;
-        display: flex;
-        align-items: flex-start;
-    `;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: flex-start;
+`;
 
 const Board = () => {
     const [{ lists }, dispatch] = useStateValue();
